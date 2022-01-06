51216bb0ca13d3ba038bc5f55e54f2b2fd5aedc0
黄彪 <191059014@qq.com>
2022-01-06 23:57:37 +0800

    优化

--- temp/test_proxy.py
+++ temp/test_proxy.py
@@ -28,11 +28,13 @@ PROXYS = [
 
 
 def invoke(proxy):
-    url = "https://www.mcmssc.com/0_69/41890.html"
+    url = "https://read.qidian.com/chapter/MUNxpctET5IuTkiRw_sFYA2/16ZAmnHutABOBDFlr9quQA2/"
     resp = requests.get(url, headers={'User-Agent': USER_AGENT}, proxies=proxy)
     status_code = resp.status_code
     if status_code != 200:
         print("code: " + str(status_code) + ', message: ' + resp.reason + ", " + url + ", " + str(proxy))
+    else:
+        print("code: " + str(status_code) + ", " + url + ", " + str(proxy))
 
 
 for proxy in PROXYS:
