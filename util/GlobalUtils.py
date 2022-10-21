1a4617dac7eee902bef0423970c6835d0942ca59
191059014 <191059014@qq.com>
2022-10-21 22:27:59 +0800

    工具类

--- util/GlobalUtils.py
+++ util/GlobalUtils.py
@@ -17,50 +17,50 @@ TIMESTAMP_SECONDS_FORMAT = '%Y%m%d%H%M%S'
 TIMESTAMP_MICROSECONDS_FORMAT = '%Y%m%d%H%M%S%f'
 
 
-def is_empty(obj):
+def isEmpty(obj):
     if obj is None:
         return True
     if obj != obj:
         return True
     if isinstance(obj, str):
-        return len(str) == 0 or obj.isspace()
+        return len(obj) == 0 or obj.isspace()
     return False
 
 
-def is_not_empty(obj):
-    return not is_empty(obj)
+def isNotEmpty(obj):
+    return not isEmpty(obj)
 
 
-def get_destop_path(filename):
+def getDestopPath(filename):
     return 'C:\\Users\\19105\\Desktop\\' + filename
 
 
-def str_to_date(date_string: str):
+def str2Date(date_string: str):
     return datetime.datetime.strptime(date_string=date_string, format=DEFAULT_DATE_FORMAT)
 
 
-def date_to_str(date_object, format: str = DEFAULT_DATE_FORMAT):
+def date2Str(date_object, format: str = DEFAULT_DATE_FORMAT):
     return date_object.strptime(format)
 
 
-def get_now_date():
+def getNowTime():
     return datetime.datetime.now()
 
 
-def get_now_date_str(format: str = DEFAULT_DATE_FORMAT):
+def getNowDateStr(format: str = DEFAULT_DATE_FORMAT):
     return datetime.datetime.now().strftime(format)
 
 
-def get_now_timestamp(format: str = TIMESTAMP_MICROSECONDS_FORMAT):
+def getNowTimestamp(format: str = TIMESTAMP_MICROSECONDS_FORMAT):
     return datetime.datetime.now().strftime(format)
 
 
-def calc_subtract_ms(start_time, end_time):
+def getTimeDiffMs(start_time, end_time):
     subtract_time = end_time - start_time
     return subtract_time.microseconds // 1000
 
 
-def http_get(url, params=None, retryTimes=0, encoding=None):
+def doHttpGet(url, params=None, retryTimes=0, encoding=None):
     """
     http的get请求
     :param url: 网页url
@@ -94,11 +94,11 @@ def http_get(url, params=None, retryTimes=0, encoding=None):
     if len(exeptions) == retryTimes + 1:
         print('请求异常：', exeptions)
     end_time = datetime.datetime.now()
-    print('请求完成，耗时=%sms，url=%s' % (calc_subtract_ms(start_time, end_time), url))
+    print('请求完成，耗时=%sms，url=%s' % (getTimeDiffMs(start_time, end_time), url))
     return response_body
 
 
-def crawl_web_page(url, retryTimes=1, encoding=None) -> BeautifulSoup | None:
+def crawlWebPage(url, retryTimes=1, encoding=None) -> BeautifulSoup | None:
     """
     爬取网页
     :param url: 网页url
@@ -106,7 +106,7 @@ def crawl_web_page(url, retryTimes=1, encoding=None) -> BeautifulSoup | None:
     :param encoding: 解析网页的编码
     :return 文档对象
     """
-    html = http_get(url, retryTimes=retryTimes, encoding=encoding)
+    html = doHttpGet(url, retryTimes=retryTimes, encoding=encoding)
     if html is not None:
         warnings.filterwarnings('ignore', category=GuessedAtParserWarning)
         return bs4.BeautifulSoup(html, parser='html.parser')
@@ -120,12 +120,12 @@ def createExcel(fileName, datas, headers=None, fileType='xlsx'):
     :param headers: 表格的表头
     """
     df = pd.DataFrame(data=datas, columns=headers)
-    filePath = get_destop_path(fileName + "_" + get_now_timestamp(TIMESTAMP_DAY_FORMAT) + "." + fileType)
+    filePath = getDestopPath(fileName + "_" + getNowTimestamp(TIMESTAMP_DAY_FORMAT) + "." + fileType)
     if fileType == 'xlsx' or fileType == 'xls':
         df.to_excel(filePath, index=False)
     else:
         df.to_csv(filePath, index=False)
-    print('创建完成，共%s行，%s' % (str(len(datas)), filePath))
+    print('创建表格完成，共%s行，%s' % (str(len(datas)), filePath))
 
 
 def readExcelAsList(filePath, sheetName='Sheet1', filterFunction=None):
@@ -139,9 +139,24 @@ def readExcelAsList(filePath, sheetName='Sheet1', filterFunction=None):
         df = pd.read_excel(filePath, sheet_name=sheetName, dtype='str')
     else:
         df = pd.read_csv(filePath, low_memory=True, dtype='str')
-    datas, total, filter_count = transfer_dataframe_and_filter(df, filterFunction)
-    print('读取完成，共%s行，过滤掉%s行，最终剩下%s行，%s' % (total, filter_count, str(len(datas)), filePath))
+    datas, total, filter_count = _transfer_dataframe_and_filter(df, filterFunction)
+    print('表格读取完成，共%s行，过滤掉%s行，最终剩下%s行，%s' % (total, filter_count, str(len(datas)), filePath))
     return datas
+
+
+def readExcelAsKvStr(filePath, sheetName='Sheet1', filterFunction=None, keyCols=None, valCols=None):
+    """
+    读取excel表格，结果为字典类型，key和value都是字符串
+    :param filePath: 完整的文件路径名
+    :param sheetName: sheet名称
+    :param filterFunction: 过滤的函数
+    :param keyCols key的列名
+    :param valCols value的列名
+    """
+    lists = readExcelAsList(filePath, sheetName, filterFunction=filterFunction)
+    dicts = _transfer_data_to_kv_str(lists, keyCols, valCols)
+    print('字典容量=%s' % len(dicts))
+    return dicts
 
 
 def readExcelAsDictDistinct(filePath, sheetName='Sheet1', keyCols=None, filterFunction=None):
@@ -152,8 +167,10 @@ def readExcelAsDictDistinct(filePath, sheetName='Sheet1', keyCols=None, filterFu
     :param keyCols: 字典的key的列名
     :param filterFunction: 过滤的函数
     """
-    datas = readExcelAsList(filePath, sheetName, filterFunction=filterFunction)
-    return transfer_data_to_dict_distinct(datas, keyCols)
+    lists = readExcelAsList(filePath, sheetName, filterFunction=filterFunction)
+    dicts = _transfer_data_to_dict_distinct(lists, keyCols)
+    print('字典容量=%s' % len(dicts))
+    return dicts
 
 
 def readExcelAsDictGroupby(filePath, sheetName='Sheet1', keyCols=None, filterFunction=None):
@@ -164,8 +181,10 @@ def readExcelAsDictGroupby(filePath, sheetName='Sheet1', keyCols=None, filterFun
     :param keyCols: 字典的key的列名
     :param filterFunction: 过滤的函数
     """
-    datas = readExcelAsList(filePath, sheetName, filterFunction=filterFunction)
-    return transfer_data_to_dict_groupby(datas, keyCols)
+    lists = readExcelAsList(filePath, sheetName, filterFunction=filterFunction)
+    dicts = _transfer_data_to_dict_groupby(lists, keyCols)
+    print('字典容量=%s' % len(dicts))
+    return dicts
 
 
 def createTextFile(fileName, lineContents, fileType='txt'):
@@ -174,10 +193,10 @@ def createTextFile(fileName, lineContents, fileType='txt'):
     :param fileName: 文件名
     :param lineContents: 所有行内容
     """
-    filePath = get_destop_path(fileName + "_" + get_now_timestamp(TIMESTAMP_DAY_FORMAT) + "." + fileType)
+    filePath = getDestopPath(fileName + "_" + getNowTimestamp(TIMESTAMP_DAY_FORMAT) + "." + fileType)
     with open(filePath, 'w') as f:
         f.write('\n'.join(lineContents))
-    print('创建完成，共%s行，%s' % (filePath, str(len(lineContents))))
+    print('创建文本完成，共%s行，%s' % (filePath, str(len(lineContents))))
 
 
 def readTextFileAsList(filePath, filterFunction=None, sep=','):
@@ -190,7 +209,7 @@ def readTextFileAsList(filePath, filterFunction=None, sep=','):
     with open(filePath, 'r', encoding='utf-8') as f:
         for line in f:
             rows.append(line)
-    datas = []
+    lists = []
     headers = rows[0].split(sep)
     filter_count = 0
     for i in range(1, len(rows)):
@@ -202,11 +221,25 @@ def readTextFileAsList(filePath, filterFunction=None, sep=','):
             value = row_data[j].strip().replace("'", '').replace('"', '')
             dicts[headerName] = value
         if filterFunction is None or filterFunction(row_data):
-            datas.append(dicts)
+            lists.append(dicts)
         else:
             filter_count += 1
-    print('读取完成，共%s行，过滤掉%s行，最终剩下%s行，%s' % (len(rows), filter_count, str(len(datas)), filePath))
-    return datas
+    print('文本读取完成，共%s行，过滤掉%s行，最终剩下%s行，%s' % (len(rows), filter_count, str(len(lists)), filePath))
+    return lists
+
+
+def readTextFileAsKvStr(filePath, filterFunction=None, keyCols=None, valCols=None):
+    """
+    读取文本文件，结果为字典类型，key和value都是字符串
+    :param filePath: 完整的文件路径名
+    :param filterFunction: 过滤的函数
+    :param keyCols key的列名
+    :param valCols value的列名
+    """
+    lists = readTextFileAsList(filePath, filterFunction=filterFunction)
+    dicts = _transfer_data_to_kv_str(lists, keyCols, valCols)
+    print('字典容量=%s' % len(dicts))
+    return dicts
 
 
 def readTextFileAsDictDistinct(filePath, keyCols=None, filterFunction=None, sep=','):
@@ -217,7 +250,9 @@ def readTextFileAsDictDistinct(filePath, keyCols=None, filterFunction=None, sep=
     :param filterFunction: 过滤的函数
     """
     datas = readTextFileAsList(filePath, filterFunction=filterFunction, sep=sep)
-    return transfer_data_to_dict_distinct(datas, keyCols)
+    dicts = _transfer_data_to_dict_distinct(datas, keyCols)
+    print('字典容量=%s' % len(dicts))
+    return dicts
 
 
 def readTextFileAsDictGroupby(filePath, keyCols=None, filterFunction=None, sep=','):
@@ -228,10 +263,39 @@ def readTextFileAsDictGroupby(filePath, keyCols=None, filterFunction=None, sep='
     :param filterFunction: 过滤的函数
     """
     datas = readTextFileAsList(filePath, filterFunction=filterFunction, sep=sep)
-    return transfer_data_to_dict_groupby(datas, keyCols)
+    dicts = _transfer_data_to_dict_groupby(datas, keyCols)
+    print('字典容量=%s' % len(dicts))
+    return dicts
 
 
-def transfer_dataframe_and_filter(df: pd.DataFrame, filterFunction=None):
+def generateBatchStr(str_template, lists):
+    """
+    通过文件批量生成字符串
+    :param str_template: 带占位符的字符串
+    :param lists: 元素为字典类型的列表
+    """
+    str_list = list(map(lambda row: str_template.format(**row), lists))
+    print('列表容量=%s' % len(str_list))
+    return str_list
+
+
+def generateBatchStrFromFile(str_template, filePath, sheetName='Sheet1', filterFunction=None, sep=','):
+    """
+    通过文件批量生成字符串
+    :param str_template: 带占位符的字符串
+    :param filePath: 完整的文件路径名
+    :param sheetName: sheet名称
+    :param filterFunction: 过滤的函数
+    :param sep: 文本内容的分隔符
+    """
+    if filePath.endswith('.xlsx') or filePath.endswith('.xls') or filePath.endswith('.csv'):
+        lists = readExcelAsList(filePath, sheetName, filterFunction)
+    else:
+        lists = readTextFileAsList(filePath, sep, filterFunction)
+    return generateBatchStr(str_template, lists)
+
+
+def _transfer_dataframe_and_filter(df: pd.DataFrame, filterFunction=None):
     columns = df.columns
     values = df.values
     datas = []
@@ -239,7 +303,9 @@ def transfer_dataframe_and_filter(df: pd.DataFrame, filterFunction=None):
     for rowNum in range(len(values)):
         rowData = {}
         for colNum in range(len(columns)):
-            rowData[columns[colNum]] = values[rowNum][colNum]
+            val = values[rowNum][colNum]
+            val = '' if isEmpty(val) else val
+            rowData[columns[colNum]] = val
         if filterFunction is None or filterFunction(rowData):
             datas.append(rowData)
         else:
@@ -247,7 +313,16 @@ def transfer_dataframe_and_filter(df: pd.DataFrame, filterFunction=None):
     return datas, len(values), filter_count
 
 
-def transfer_data_to_dict_distinct(datas, keyCols):
+def _transfer_data_to_kv_str(datas, keyCols, valCols):
+    dicts = {}
+    for row in datas:
+        key = '_'.join(map(lambda keyColName: row[keyColName], keyCols))
+        value = '_'.join(map(lambda valColName: row[valColName], valCols))
+        dicts[key] = value
+    return dicts
+
+
+def _transfer_data_to_dict_distinct(datas, keyCols):
     dicts = {}
     for row in datas:
         key = '_'.join(map(lambda keyColName: row[keyColName], keyCols))
@@ -255,7 +330,7 @@ def transfer_data_to_dict_distinct(datas, keyCols):
     return dicts
 
 
-def transfer_data_to_dict_groupby(datas, keyCols):
+def _transfer_data_to_dict_groupby(datas, keyCols):
     dicts = {}
     for row in datas:
         key = '_'.join(map(lambda keyColName: row[keyColName], keyCols))
@@ -275,6 +350,10 @@ if __name__ == '__main__':
     # data = readExcelAsList(get_destop_path('tb_book.csv'))
     # print(data)
     # createExcel('ceshi2', [['123', '您好']], headers=['age', '姓名'], fileType='xls')
-    # data = readTextFileAsList(get_destop_path('tb_book.txt'))
+    data = readTextFileAsList(getDestopPath('tb_book.txt'))
     # print(data)
-    createTextFile('ces', ['123', '哈哈'])
+    # createTextFile('ces', ['123', '哈哈'])
+    # print(readExcelAsKvStr(getDestopPath('tb_book.csv'), keyCols=['id'], valCols=['name']))
+    # print(generateBatchStr('select {name} from dual',[{'name':'zhangsan'},{'name':'lisi'}]))
+    # print(generateBatchStr('select {name} from dual', [{'name': 'zhangsan'}, {'name': 'lisi'}]))
+    print(generateBatchStrFromFile('select {name} from dual', getDestopPath('tb_book.csv')))
