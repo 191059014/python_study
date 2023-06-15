7d10d4a966a6f7f6a83a07bf1cd4aaeac628e15c
huangbiao <191059014@qq.com>
2023-06-15 23:05:53 +0800

    aaa

--- util/GlobalUtils.py
+++ util/GlobalUtils.py
@@ -8,6 +8,7 @@ import requests
 from lxml import etree
 
 
+# obj工具类
 class ObjUtils():
     @staticmethod
     def isEmpty(obj):
@@ -26,6 +27,29 @@ class ObjUtils():
         return not ObjUtils.isEmpty(obj)
 
 
+# 字符串工具类
+class StrUtils():
+    @staticmethod
+    def joinUnderline(*vals):
+        return "_".join(vals)
+
+    @staticmethod
+    def wrap(origin, symbol):
+        return symbol + origin + symbol
+
+    @staticmethod
+    def formatBatch(str_template, lists):
+        """
+        通过文件批量生成字符串
+        :param str_template: 带占位符的字符串
+        :param lists: 元素为字典类型的列表
+        """
+        str_list = list(map(lambda row: str_template.format(**row), lists))
+        print('formatBatch列表容量=%s' % len(str_list))
+        return str_list
+
+
+# 日期工具类
 class DateUtils():
     # 默认时间格式
     DEFAULT_FORMAT = '%Y-%m-%d %H:%M:%S'
@@ -64,6 +88,7 @@ class DateUtils():
         return subtract_time.microseconds // 1000
 
 
+# http工具类
 class HttpUtils():
     @staticmethod
     def get(url, params=None, headers=None, encoding=None) -> str:
@@ -85,8 +110,9 @@ class HttpUtils():
         return content
 
 
+# 爬虫工具类
 class SpiderUtils():
-    def crawlWebPage(url, encoding=None) -> etree._Element | None:
+    def doCrawling(url, encoding=None) -> etree._Element | None:
         """
         爬取网页
         :param url: 网页url
@@ -98,13 +124,15 @@ class SpiderUtils():
             return etree.HTML(content)
 
 
+# 数据库工具
 class DB():
     _db_conn = None
     _env = None
 
     def __init__(self, env='local') -> None:
         env_maps = {
-            "local": ['127.0.0.1', 3306, 'test', 'root', 'root3306']
+            "local": ['127.0.0.1', 3306, 'test', 'root', 'root3306'],
+            "dev": ['127.0.0.1', 3306, 'dev', 'root', 'root3306']
         }
         conf = env_maps.get(env)
         if not conf:
@@ -113,36 +141,28 @@ class DB():
         self._db_conn = pymysql.connect(host=conf[0], port=conf[1], database=conf[2], user=conf[3], password=conf[4])
 
     def query(self, sql, args=None) -> list:
-        """
-        查询
-        :param sql: sql语句
-        :param args: 参数
-        :return 数据列表
-        """
         cursor = self._db_conn.cursor(cursor=pymysql.cursors.DictCursor)
         cursor.execute(sql, args)
-        return cursor.fetchall()
+        rows = cursor.fetchall()
+        cursor.close()
+        return rows
 
     def execute(self, sql, args=None, auto_commit=True) -> int:
-        """
-        新增、修改、删除
-        :param sql: sql语句
-        :param args: 参数
-        :return 影响的行数
-        """
         cursor = self._db_conn.cursor()
         affect_number = cursor.execute(sql, args)
         if auto_commit:
             self.commit()
+        cursor.close()
         return affect_number
 
     def commit(self):
         self._db_conn.commit()
         print('事务已提交')
 
-    def rollback(self):
+    def rollback(self, e):
         self._db_conn.rollback()
-        print('事务已回滚')
+        print('事务已回滚，异常信息如下：')
+        print(e)
 
     def __del__(self):
         """
@@ -152,7 +172,39 @@ class DB():
             self._db_conn.close()
             print('%s数据库连接已关闭' % self._env)
 
+    @staticmethod
+    def migrate(originEnv, targetEnv, tableName, condition='1=1', excludeColumns=[]):
+        start = datetime.datetime.now()
+        # 获取待迁移的数据
+        originDb = DB(originEnv)
+        originQuerySql = 'select * from %s where %s' % (tableName, condition)
+        originCursor = originDb._db_conn.cursor(cursor=pymysql.cursors.DictCursor)
+        originCursor.execute(originQuerySql)
+        originRowDatas = originCursor.fetchall()
+        # 遍历所有行，一行一行插入
+        targetDb = DB(targetEnv)
+        try:
+            for originRowData in originRowDatas:
+                # 过滤掉值为空以及制定排除的列
+                effectColumns = tuple(
+                    filter(lambda item: item[1] is not None and item[0] not in excludeColumns, originRowData.items()))
+                # 列名前后加反引号
+                columns = [StrUtils.wrap(k, '`') for k, v in effectColumns]
+                # 非数字类型的加单引号
+                values = [str(v) if isinstance(v, (int, float)) else StrUtils.wrap(v, "'") for k, v in effectColumns]
+                # 组装插入语句
+                targetInsertSql = 'insert into %s (%s) values (%s)' % (tableName, ','.join(columns), ','.join(values))
+                print(targetInsertSql)
+                targetDb.execute(targetInsertSql, auto_commit=False)
+            targetDb.commit()
+            end = datetime.datetime.now()
+            cost = DateUtils.getTimeDiffMs(start, end)
+            print('数据迁移完成，%s->%s，总共%s行，总共耗时：%s秒' % (originEnv, targetEnv, len(originRowDatas), cost))
+        except Exception as e:
+            targetDb.rollback(e)
 
+
+# excel、csv工具类
 class ExcelUtils():
 
     @staticmethod
@@ -206,8 +258,8 @@ class ExcelUtils():
         rows = ExcelUtils.readAsList(filePath, sheetIndex, rowFilter=rowFilter)
         dicts = {}
         for row in rows:
-            key = joinUnderline(*map(lambda keyColName: row.get(keyColName), keyCols))
-            value = joinUnderline(*map(lambda valColName: row.get(valColName), valCols))
+            key = StrUtils.joinUnderline(*map(lambda keyColName: row.get(keyColName), keyCols))
+            value = StrUtils.joinUnderline(*map(lambda valColName: row.get(valColName), valCols))
             dicts[key] = value
         print('readExcelAsKvStr字典容量=%s' % len(dicts))
         return dicts
@@ -222,7 +274,7 @@ class ExcelUtils():
         :param rowFilter: 过滤的函数
         """
         rows = ExcelUtils.readAsList(filePath, sheetIndex, rowFilter=rowFilter)
-        dicts = {joinUnderline(*map(lambda keyColName: row.get(keyColName), keyCols)): row for row in rows}
+        dicts = {StrUtils.joinUnderline(*map(lambda keyColName: row.get(keyColName), keyCols)): row for row in rows}
         print('readExcelAsDictDistinct字典容量=%s' % len(dicts))
         return dicts
 
@@ -238,7 +290,7 @@ class ExcelUtils():
         rows = ExcelUtils.readAsList(filePath, sheetIndex, rowFilter=rowFilter)
         dicts = {}
         for row in rows:
-            key = joinUnderline(*map(lambda keyColName: row.get(keyColName), keyCols))
+            key = StrUtils.joinUnderline(*map(lambda keyColName: row.get(keyColName), keyCols))
             if key in dicts:
                 dicts[key].extend([row])
             else:
@@ -257,9 +309,10 @@ class ExcelUtils():
         """
         lists = splitList(datas, size)
         for i in range(len(lists)):
-            ExcelUtils.create(joinUnderline(fileName, str(i + 1)), lists[i])
+            ExcelUtils.create(StrUtils.joinUnderline(fileName, str(i + 1)), lists[i])
 
 
+# 文本文件工具类
 class TextUtils():
     @staticmethod
     def create(fileName, lineContents, fileType='sql'):
@@ -277,13 +330,18 @@ class TextUtils():
 
     @staticmethod
     def read(filePath) -> str:
+        """
+        读取文本文件，返回一个完整的字符串
+        :param filePath: 文件完整路径名
+        :return 文件内容
+        """
         with open(filePath, 'r', encoding='utf-8') as f:
             return f.read()
 
     @staticmethod
     def readAsList(filePath) -> list:
         """
-        读取文本文件
+        读取文本文件，每一行作为集合的一个元素
         :param filePath: 文件完整路径名
         :return 文件内容
         """
@@ -296,45 +354,35 @@ class TextUtils():
         return rows
 
 
+class ListUtils():
+    @staticmethod
+    def split(dataList, size):
+        """
+        将一个大的列表按固定size分割成若干个小列表
+        :param dataList: 列表
+        :param size: 固定长度
+        :return 包含若干个小列表的列表
+        """
+        total = len(dataList)
+        count, mod = divmod(total, size)
+        if mod > 0:
+            count += 1
+        return [dataList[i * size:(i + 1) * size] for i in range(count)]
+
+
+# 获取桌面路径
 def getDestopPath(filename):
     return os.path.join(os.path.expanduser("~"), 'Desktop') + '\\' + filename
 
 
+# 获取默认下载路径
 def getDownloadPath(filename):
     return os.path.join(os.path.expanduser("~"), 'Downloads') + '\\' + filename
 
 
-def joinUnderline(*vals):
-    return "_".join(vals)
-
-
-def splitList(dataList, size):
-    """
-    将一个大的列表按固定size分割成若干个小列表
-    :param dataList: 列表
-    :param size: 固定长度
-    :return 包含若干个小列表的列表
-    """
-    total = len(dataList)
-    count, mod = divmod(total, size)
-    if mod > 0:
-        count += 1
-    return [dataList[i * size:(i + 1) * size] for i in range(count)]
-
-
-def formatStrBatch(str_template, lists):
-    """
-    通过文件批量生成字符串
-    :param str_template: 带占位符的字符串
-    :param lists: 元素为字典类型的列表
-    """
-    str_list = list(map(lambda row: str_template.format(**row), lists))
-    print('formatStrBatch列表容量=%s' % len(str_list))
-    return str_list
-
-
 if __name__ == '__main__':
     # print(ExcelUtils.readAsDictGroupby(getDestopPath('次新股_20230609.xlsx'), keyCols=['名称', '代码']))
-    db = DB()
-    rownumber = db.execute("update t_user set password='1234567' where id>1")
-    print(rownumber)
+    # db = DB()
+    # rownumber = db.execute("update t_user set password='1234567' where id>1")
+    # print(rownumber)
+    DB.migrate('local', 'dev', 't_user', excludeColumns=['password'])
